212871843f2648f3b48742510f9b912002e6bc26
hanane ek <132601446+hanhoun127@users.noreply.github.com>
2023-12-08 21:20:36 +0100

    Add files via upload

--- project/src/Project.java
+++ project/src/Project.java
@@ -6,98 +6,134 @@ import java.awt.event.*;
 
 
 public class Project extends JFrame {
-    private JPanel cardPanel;
-    private CardLayout cardLayout;
-
+    CardLayout cardLayout;
+    JPanel stdPan,admPan,studentPanel,adminPanel,homePanel,cardPanel,studentPan,adminPan,searchPanel;
+    JPanel loginPan;
+    JButton studentButton,adminButton,homeButton,searchBtn,loginBtn;
+    JLabel stdLab,admLab,searchLab,idLab,passwordLab,loginLab;
+    ImageIcon stdIcon,admIcon;
+    JTextField searchTf,idTf;
+    JPasswordField passwordTp;
     public Project() {
         setTitle("Home");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(550, 325);
 
-        // Create card panel with CardLayout
         cardPanel = new JPanel();
         cardLayout = new CardLayout();
         cardPanel.setLayout(cardLayout);
 
-        // Create Student panel
-        JPanel studentPanel = createPanel("Student Panel");
+        studentPanel = createStudentPanel();
         addNavigationButton(studentPanel, "studentPanel");
 
-        // Create Admin panel
-        JPanel adminPanel = createPanel("Admin Panel");
+        adminPanel = createAdmintPanel();
         addNavigationButton(adminPanel, "adminPanel");
 
-        // Create Home panel with buttons to switch between Student and Admin panels
-        JPanel homePanel = createHomePanel();
+        homePanel = createHomePanel();
 
-        // Add panels to the card panel
         cardPanel.add(studentPanel, "studentPanel");
         cardPanel.add(adminPanel, "adminPanel");
         cardPanel.add(homePanel, "homePanel");
 
-        // Add components to the main frame
         add(cardPanel, BorderLayout.CENTER);
 
-        // Set default panel
         cardLayout.show(cardPanel, "homePanel");
         setResizable(false);
         setVisible(true);
     }
 
-    private JPanel createPanel(String title) {
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-
-        JLabel label = new JLabel(title, SwingConstants.CENTER);
-        label.setFont(new Font("Arial", Font.BOLD, 24));
-        panel.add(label, BorderLayout.CENTER);
-
-        return panel;
+     public JPanel createStudentPanel() {
+        studentPan=new JPanel(new BorderLayout());
+        searchPanel=new JPanel();
+        searchLab=new JLabel("search memoire");
+        searchPanel.add(searchLab);
+        searchTf=new JTextField(30);
+        searchPanel.add(searchTf);
+        searchBtn=new JButton("search");
+        searchPanel.add(searchBtn);
+        studentPan.add(searchPanel);
+        return studentPan;
     }
 
-    private JPanel createHomePanel() {
-        JPanel homePanel = new JPanel(new BorderLayout());
-        JButton studentButton = new JButton("                 GO To Student Space                  ");
-        ImageIcon stdIcon=new ImageIcon(getClass().getResource("img/student.png"));
-        JLabel stdLab=new JLabel(stdIcon);
+    public JPanel createAdmintPanel() {
+        adminPan=new JPanel(new BorderLayout());
+        loginPan=new JPanel();
+        idLab=new JLabel("Enter your ID");
+        loginPan.add(idLab);
+        idTf=new JTextField();
+        loginPan.add(idTf);
+        passwordLab=new JLabel("Enter your password");
+        loginPan.add(passwordLab);
+        passwordTp=new JPasswordField();
+        loginPan.add(passwordTp);
+        loginBtn=new JButton("Login");
+        
+        loginPan.add(loginBtn);
+        loginPan.setLayout(new BoxLayout(loginPan,BoxLayout.Y_AXIS));
+        adminPan.add(loginPan);
+        loginLab=new JLabel(" ");
+        adminPan.add(loginLab,BorderLayout.SOUTH);
+        loginBtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ev){
+                String Password=new String (passwordTp.getPassword());
+                if(!idTf.getText().equals("1HN2MR2004") || !Password.equals("a1s2d3f4")){
+                    loginLab.setText("the id or the password incorrect! try agin");
+                }else{
+                    loginLab.setText("enter");
+                }
+            }
+        });
+        return adminPan;
+    }
+    
+    public JPanel createHomePanel() {
+        homePanel  = new JPanel(new BorderLayout());
+        studentButton = new JButton("                 GO To Student Space                  ");
+        stdIcon=new ImageIcon(getClass().getResource("img/student.png"));
+        stdLab=new JLabel(stdIcon);
         studentButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 cardLayout.show(cardPanel, "studentPanel");
+                setTitle("Student Space");
             }
         });
 
-        JButton adminButton = new JButton("                  GO To Admin Space                      ");
-        ImageIcon admIcon=new ImageIcon(getClass().getResource("img/admin.png"));
-        JLabel admLab=new JLabel(admIcon);
+        adminButton = new JButton("                  GO To Admin Space                      ");
+        admIcon=new ImageIcon(getClass().getResource("img/admin.png"));
+        admLab=new JLabel(admIcon);
         adminButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 cardLayout.show(cardPanel, "adminPanel");
+                setTitle("Admin Space");
+                setSize(350, 200);
             }
         });
-        JPanel stdPan=new JPanel();
+        stdPan=new JPanel();
         stdPan.add(stdLab);
         stdPan.add(studentButton); 
         stdPan.setBorder(new LineBorder(Color.BLACK, 2));
         stdPan.setLayout(new BoxLayout(stdPan,BoxLayout.Y_AXIS));
-        JPanel admPan=new JPanel();
+        admPan=new JPanel();
         admPan.add(admLab);
         admPan.add(adminButton);
         admPan.setBorder(new LineBorder(Color.BLACK, 2));
         admPan.setLayout(new BoxLayout(admPan,BoxLayout.Y_AXIS));
         homePanel.add(stdPan,BorderLayout.WEST);
-        homePanel.add(admPan,BorderLayout.CENTER);
+        homePanel.add(admPan,BorderLayout.EAST);
 
         return homePanel;
     }
 
-    private void addNavigationButton(JPanel panel, String targetPanel) {
-        JButton homeButton = new JButton("Home");
+    public void addNavigationButton(JPanel panel, String targetPanel) {
+        homeButton = new JButton("Home");
         homeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 cardLayout.show(cardPanel, "homePanel");
+                setSize(550, 325);
             }
         });
 
