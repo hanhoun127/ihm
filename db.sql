08fa2e3715bac33bf9d6eb7cb5c43d5abdaba824
hanane ek <132601446+hanhoun127@users.noreply.github.com>
2023-12-13 20:31:49 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,24 +1,25 @@
 DROP DATABASE IF EXISTS ihm ;
 CREATE DATABASE ihm;
 use ihm
-
+CREATE TABLE teacher (
+    id INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL ,
+    email VARCHAR (255) NOT NULL,
+    specialty VARCHAR(100)
+);
 CREATE TABLE memoire (
     referenceNbr VARCHAR(20) PRIMARY KEY NOT NULL ,
+    id_teacher INT  NOT NULL,
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     year YEAR NOT NULL,
     supervisor CHAR(25) NOT NULL,
     summary TEXT,
-    FOREIGN KEY (supervisor) REFERENCES teacher(name)
+    FOREIGN KEY (id_teacher) REFERENCES teacher(id)
     
 );
 
-CREATE TABLE teacher (
-    id INT  NOT NULL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL ,
-    email VARCHAR (255) NOT NULL,
-    specialty VARCHAR(100)
-);
+
 
 INSERT INTO `teacher`( `name`, `email`, `specialty`) VALUES ('Rezoug Abdellah','Abdellah.rezoug@gmail.com','Interaction Homme-Machine');
 INSERT INTO `teacher`( `name`, `email`, `specialty`) VALUES ('Boulif Menouar','boumen7@gmail.com','Intelligence Artificielle ');
