5fd22d550a791375b001e680bafa819f3b464b01
04xmk <150384698+04xmk@users.noreply.github.com>
2023-12-13 11:20:37 +0100

    Add files via upload

--- db.sql
+++ db.sql
@@ -2,21 +2,23 @@ DROP DATABASE IF EXISTS ihm ;
 CREATE DATABASE ihm;
 use ihm
 
+CREATE TABLE teacher (
+    id VARCHAR(25)  NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR (255) NOT NULL,
+    specialty VARCHAR(100)
+);
+
 CREATE TABLE memoire (
     referenceNbr VARCHAR(20) PRIMARY KEY NOT NULL ,
+    id_teacher VARCHAR(25) NOT NULL,
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     year YEAR NOT NULL,
     supervisor CHAR(25) NOT NULL,
     summary TEXT,
-    FOREIGN KEY (supervisor) REFERENCES teacher(name)
+    FOREIGN KEY (id_teacher) REFERENCES teacher(id)
     
 );
 
-CREATE TABLE teacher (
-    id INT  NOT NULL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL ,
-    email VARCHAR (255) NOT NULL,
-    specialty VARCHAR(100)
-);
 
