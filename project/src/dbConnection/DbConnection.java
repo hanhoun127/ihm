b337419e294bff85a4f37312360899639fdc70ab
hanane ek <132601446+hanhoun127@users.noreply.github.com>
2023-12-15 12:33:21 +0100

    Update DbConnection.java

--- project/src/dbConnection/DbConnection.java
+++ project/src/dbConnection/DbConnection.java
@@ -1,12 +1,27 @@
 package dbConnection;
 import java.sql.*;
+
+import javax.swing.JOptionPane;
 public class DbConnection {
     private static Connection con;
     private static String URL="jdbc:mysql://localhost:3306/ihm";
     private static String USER="root";
     private static String PASSWORD="";
     // methode to etablish connection to database
-    public static Connection connect() throws SQLException {
-    return DriverManager.getConnection(URL,USER,PASSWORD);
+    public static Connection connect() {
+    
+    try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); 
+			con= DriverManager.getConnection(URL,USER,PASSWORD);
+			
+		} catch (ClassNotFoundException e) {
+			JOptionPane.showMessageDialog(null, e);
+		}
+		catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, e);
+		}
+    return con;
     }
+
+
 }
